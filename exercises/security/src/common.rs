9331a45295d3081f13420175a5de37389034f749
harryng0312 <little.ruan@gmail.com>
2025-04-02 23:52:11 +0700

    init socket

--- exercises/security/src/common.rs
+++ exercises/security/src/common.rs
@@ -23,23 +23,30 @@ pub fn gen_random_byte_arr(rand_v: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
     // let mut thread_rng = rand::thread_rng();
     // let mut rand_v: Vec<u8>= vec![0u8; arr_len];
     // thread_rng.fill(&mut *rand_v);
-    rand_bytes(&mut *rand_v).unwrap();
-    Ok(())
+    rand_bytes(&mut *rand_v).map_err(|e| Box::new(e) as Box<dyn Error>)
+    // Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use crate::common::{from_base64, gen_random_byte_arr, to_base64};
     use log::info;
+    // use log::kv::ToValue;
     use utils::log::configuration::init_logger;
 
     #[test]
     fn test_gen_random_byte_arr() {
         init_logger();
         let mut bin_data: Vec<u8> = vec![0u8; 32];
-        gen_random_byte_arr(&mut bin_data).unwrap();
-        let b64_data = to_base64(bin_data.as_slice()).unwrap();
-        info!("b64 data:{}", b64_data);
+        if let Ok(_) = gen_random_byte_arr(&mut bin_data) {
+            if let Ok(b64_data) = to_base64(bin_data.as_slice()) {
+                info!("b64 data:{}", b64_data);
+            } else {
+                info!("Error converting data to base64");
+            }
+        } else {
+            info!("Error getting random bytes");
+        }
     }
 
     #[test]
