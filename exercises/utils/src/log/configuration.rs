ab8a7dfaedff59f2e43558c77535475efc2c35ac
harryng0312 <little.ruan@gmail.com>
2025-09-08 23:23:59 +0700

    macro: declarative and procedural (except derive macro)

--- exercises/utils/src/log/configuration.rs
+++ exercises/utils/src/log/configuration.rs
@@ -1,11 +1,13 @@
 use std::env;
 use std::sync::OnceLock;
+use tracing::subscriber;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling;
 use tracing_appender::rolling::Rotation;
+use tracing_subscriber::fmt::Layer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{fmt, EnvFilter};
+use tracing_subscriber::{fmt, EnvFilter, Registry};
 
 const CONFIG_FILE: &str = "config.toml";
 // static INIT: Once = Once::new();
@@ -33,50 +35,17 @@ pub fn init_logger() {
     let log_path = env::var("LOG_PATH").unwrap_or(String::from("./logs"));
     let log_file = env::var("LOG_FILE").unwrap_or(String::from("not_config"));
 
-    // let file_appender = rolling::daily(log_path, log_file);
-    let file_appender = rolling::Builder::new()
-        .rotation(Rotation::DAILY)
-        .filename_prefix(log_file)
-        .filename_suffix("log")
-        .max_log_files(5)
-        .build(log_path)
-        .expect("build rolling appender");
-
+    let mut layers: Vec<Box<dyn tracing_subscriber::Layer<Registry> + Send + Sync>> = vec![];
     let mut guards: Vec<WorkerGuard> = vec![];
-    let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
-    guards.push(_guard);
+
     let (stdout, _guard) = tracing_appender::non_blocking(std::io::stdout());
     guards.push(_guard);
-
-    let _ = GUARDS.get_or_init(|| guards);
-
-    // Bridge log crate -> tracing
-    // tracing_log::LogTracer::init().expect("Failed to set LogTracer");
-    // Init subscriber
-    // tracing_subscriber::fmt()
-    //     .with_writer(stdout)
-    //     .with_writer(file_writer)
-    //     .init();
-
-    // let subscriber = tracing_subscriber::registry()
-    //     .with(tracing_subscriber::filter::EnvFilter::from_default_env())
-    //     .with(tracing_subscriber::fmt::layer().with_writer(stdout))
-    //     .with(tracing_subscriber::fmt::layer().with_writer(file_writer));
-    // subscriber.init();
-
-    // let subscriber = tracing_subscriber::fmt()
-    //     .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
-    //     .with_test_writer() // ghi ra stdout/stderr khi test
-    //     .with_writer(stdout)
-    //     .with_writer(file_writer)
-    //     .finish();
-    // let _ = tracing::subscriber::set_global_default(subscriber);
 
     let env_filter = std::env::var("RUST_LOG")
         .ok()
         .map(EnvFilter::new)
         .unwrap_or_else(|| EnvFilter::new("info"));
-
+    layers.push(Box::new(env_filter));
     let fmt_stdout = fmt::layer()
         .with_target(true)
         .with_thread_names(true)
@@ -86,17 +55,38 @@ pub fn init_logger() {
         .with_writer(stdout)
         .with_test_writer();
 
-    let fmt_file = fmt::layer()
-        .with_ansi(false)
-        .with_target(true)
-        .with_thread_names(true)
-        .with_line_number(true)
-        .with_file(true)
-        .with_writer(file_writer);
+    layers.push(Box::new(fmt_stdout));
 
-    tracing_subscriber::registry()
-        .with(env_filter)
-        .with(fmt_stdout)
-        .with(fmt_file)
-        .init();
+    if cfg!(test) {
+        println!("Run with `RUST_LOG={}=debug` test", run_env);
+    }
+
+    if run_env.to_lowercase() == "test" {
+        // do nothing
+    } else {
+        let file_appender = rolling::Builder::new()
+            .rotation(Rotation::DAILY)
+            .filename_prefix(log_file)
+            .filename_suffix("log")
+            .max_log_files(5)
+            .build(log_path)
+            .expect("build rolling appender");
+        let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
+        guards.push(_guard);
+        let fmt_file = fmt::layer()
+            .with_ansi(false)
+            .with_target(true)
+            .with_thread_names(true)
+            .with_line_number(true)
+            .with_file(true)
+            .with_writer(file_writer);
+        layers.push(Box::new(fmt_file));
+    }
+    // tracing_subscriber::registry()
+    //     .with(env_filter)
+    //     .with(fmt_stdout)
+    //     .with(fmt_file)
+    //     .init();
+    let _ = GUARDS.get_or_init(|| guards);
+    tracing_subscriber::registry().with(layers).init();
 }
--- exercises/web/src/main.rs
+++ exercises/web/src/main.rs
@@ -1,6 +1,8 @@
 // #![allow(warnings)]
 #![allow(clippy::too_many_arguments, unused_variables, dead_code)]
 
+use std::time::Duration;
+use tokio::time::sleep;
 use tracing::info;
 use ::utils::log::configuration::init_logger;
 
@@ -20,4 +22,15 @@ fn main() {
     // let rec = TestRecord::new(0, String::default(), false, Default::default());
     info!("Test Struct");
     // sleep(Duration::from_secs(5));
+    // let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .worker_threads(4)
+        .enable_all()
+        .build()
+        .unwrap();
+    let rs = rt.block_on(async move {
+        sleep(Duration::from_secs(5)).await;
+        1
+    });
+    info!("{}", rs);
 }
